486fa7f2b589c11e31376fc31b4a58e221200f3d
Oladapo Kayode Abiodun <141229621+oladapokayodeabiodun@users.noreply.github.com>
2026-05-13 00:38:39 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+VERIFY_TOKEN = "Training@1089"
+
+
+@app.route('/')
+def webhook():
+    return 'Training Chat is Connected'
+
+
+@app.route('/webhook', methods=['GET'])
+def verify():
+    mode = request.args.get("hub.mode")
+    token = request.args.get("hub.verify_token")
+    challenge = request.args.get("hub.challenge")
+
+    if mode and token:
+        if mode == "subscribe" and token == VERIFY_TOKEN:
+            return challenge, 200
+        else:
+            return "Verification failed", 403
+
+    return "Welcome webhook", 200
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = request.get_json()
+
+    print("Webhook Event:")
+    print(data)
+
+    return "EVENT RECEIVED", 200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
